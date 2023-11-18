3751441d41bd955255153701f21f4e7334421f82
Lisien <lisienskenderi@hotmail.com>
2023-11-18 22:21:33 +0100

    Script python aggiornato, aggiunto alcun commento, puo` gestire file senza changelog

--- Version_finder.py
+++ Version_finder.py
@@ -2,7 +2,7 @@ import re, os, sys
 
 def delete_file(file_path):
     try:
-        if "Verbale" in file_path:
+        if "Verbale" in file_path or get_first_cell_value(sys.argv[1]):
             new_file_path = file_path[:-4]
         else:
             new_file_path = file_path[:-11]
@@ -33,6 +33,7 @@ def get_first_cell_value(filename):
         match = re.findall(r'{}(.*?){}'.format("label{Git_Action_Version}", "&"), data, re.DOTALL)
         return match[0] if match else None
 
+# This functions renames the files that should have the version on the name. It gets the version number from the Latex tag {Git_Action_Version} which should be the first line in the changelog of said file
 def rename_latex_file(filename):
     filename_abs = "Documents/" + filename
     filename_abs = os.path.join(os.getcwd(), filename_abs)
@@ -49,10 +50,14 @@ def rename_latex_file(filename):
 def main():
     if ".tex" not in sys.argv[1]:
         return None
-    if "Verbale" in sys.argv[1]:
-        delete_file(sys.argv[1])
+    # if "Verbale" in sys.argv[1]:
+    #     delete_file(sys.argv[1])
     else:
         delete_file(sys.argv[1])
-        rename_latex_file(sys.argv[1])
+        if(get_first_cell_value(sys.argv[1])):
+            rename_latex_file(sys.argv[1])
 if __name__ == "__main__":
     main()
+# The python code renames Latex files that should have the version number on the name of the file by getting it from the changelog table on the latex file.
+# The python code is needed for deleting old pdf files that would have been compiled from the same github workflow that is used to call said script.
+# If this is the first time the pdf has been created than the delete isn't needed
